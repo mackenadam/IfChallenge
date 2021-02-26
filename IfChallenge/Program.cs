e05213f9ba92652981b29162445ddaa24cbcb006
Adam Macken <mackenadam@gmail.com>
2021-02-26 16:01:17 +1300

    Add project files.

---
+++ IfChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/* Create a user Login System, where the user can first register and then Login in. The Program should check if the user has entered the correct username and password, wenn login in (so the same ones that he used when registering).
+As we haven't covered storing data yet, just create the program in a way, that registering and logging in, happen in the same execution of it.
+User If statements and User Input and Methods to solve the challenge. */
+
+namespace IfChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
